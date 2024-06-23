d1849f0f89bb736c169c97cc107b458189e5a294
Dishank Poddar <dishankpoddar@gmail.com>
2024-06-23 07:20:55 -0400

    YT connection and playback added

--- soundboard.js
+++ soundboard.js
@@ -38,7 +38,8 @@ async function getYouTubePlaylistVideos(playlistId, apiKey) {
 
 let params = new URLSearchParams(document.location.search);
 const playlistId = params.get("list");
-const apiKey = '<API-KEY>';
+const apiKey = params.get("api");
+let playerList = new Object();
 
 getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
     let board = document.querySelector('#board');
@@ -51,7 +52,6 @@ getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
             let index = i*4 + j;
             if (index < videos.length) {
                 const video = videos[index];
-                // const element = array[index];
                 let videoElement = 
                 `<div class="card text-white bg-dark mb-3">
                     <span id="player-${video.id}"></span>
@@ -59,7 +59,7 @@ getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
                     <div class="card-img-overlay d-flex flex-column">
                         <p class="card-title mb-auto">${video.title}</p>
                     <div class="card-body d-flex align-items-end justify-content-around">
-                        <button type="button" class="btn btn-dark">
+                        <button type="button" class="btn btn-dark" onclick="playpauseVideo('${video.id}')">
                             <span id="play-pause-${video.id}">
                                 <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
                                 <!--
@@ -67,14 +67,14 @@ getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
                                 -->
                             </span>
                         </button>
-                        <button type="button" class="btn btn-dark">
+                        <button type="button" class="btn btn-dark" onclick="stopVideo('${video.id}')">
                             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 6h12v12H6z"/></svg>
                         </button>
-                        <button type="button" class="btn btn-dark">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
-                        </button>
-                        <button type="button" class="btn btn-dark">
+                        <button type="button" class="btn btn-dark" onclick="volumeDown('${video.id}')">
                             <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/></svg>
+                        </button>
+                        <button type="button" class="btn btn-dark" onclick="volumeUp('${video.id}')">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
                         </button>
                         <!-- <span><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg></span> -->
                     </div>
@@ -90,4 +90,60 @@ getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
         row += `</div>`;
         board.innerHTML += row;
     }
+    videos.forEach(video => {
+        playerList[video.id] = new YT.Player(`player-${video.id}`, {
+            height: '0',
+            width: '0',
+            videoId: video.id,
+            playerVars: {
+                loop: 1,
+                playlist: video.id,
+                rel: 0,
+                controls: 0,
+                disablekb: 1,
+            },
+            events: {
+                'onReady': onPlayerReady,
+            },
+          });
+    });
 });
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.stopVideo(); 
+  event.target.setVolume(50);
+}
+
+
+// Control a video's playback.
+function playpauseVideo(video_id){
+    let player = playerList[video_id];
+    let playpauseIcon = document.querySelector(`#play-pause-${video_id}`);
+    if ( player.getPlayerState() == YT.PlayerState.PLAYING || player.getPlayerState() == YT.PlayerState.BUFFERING ) {
+        player.pauseVideo();
+        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
+    } 
+    else {
+        player.playVideo();  
+        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>`;
+    } 
+}
+function stopVideo(video_id){
+    let player = playerList[video_id];
+    player.stopVideo();
+    let playpauseIcon = document.querySelector(`#play-pause-${video_id}`);
+    playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
+}
+function volumeUp(video_id){
+    let player = playerList[video_id];
+    let vol = player.getVolume();
+    player.setVolume(vol+5);
+}
+
+function volumeDown(video_id){
+    let player = playerList[video_id];
+    let vol = player.getVolume();
+    player.setVolume(vol-5);
+}
