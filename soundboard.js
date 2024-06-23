a08ab07336c380495d7263083f2713ac06a95af4
Dishank Poddar <dishankpoddar@gmail.com>
2024-06-23 06:33:01 -0400

    Add Playlist videos to screen

--- soundboard.js
+++ soundboard.js
@@ -8,33 +8,32 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Get playlist videos
 async function getYouTubePlaylistVideos(playlistId, apiKey) {
-  const baseUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${apiKey}`;
-  console.log(baseUrl)
-  let nextPageToken = '';
-  const videos = [];
+    const baseUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${apiKey}`;
+    let nextPageToken = '';
+    const videos = [];
 
-  try {
-    do {
-      const url = baseUrl + (nextPageToken ? `&pageToken=${nextPageToken}` : '');
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        for (const item of data.items) {
-          const videoTitle = item.snippet.title;
-          const videoId = item.snippet.resourceId.videoId;
-          videos.push({ title: videoTitle, id: videoId });
+    try {
+        do {
+            const url = baseUrl + (nextPageToken ? `&pageToken=${nextPageToken}` : '');
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                for (const item of data.items) {
+                const videoTitle = item.snippet.title;
+                const videoId = item.snippet.resourceId.videoId;
+                videos.push({ title: videoTitle, id: videoId });
+                }
+                nextPageToken = data.nextPageToken || '';
+            } else {
+                console.error(`Failed to retrieve playlist details: ${response.status}`);
+                break;
         }
-        nextPageToken = data.nextPageToken || '';
-      } else {
-        console.error(`Failed to retrieve playlist details: ${response.status}`);
-        break;
-      }
-    } while (nextPageToken);
-  } catch (error) {
-    console.error(`Error fetching playlist details: ${error}`);
-  }
+        } while (nextPageToken);
+    } catch (error) {
+        console.error(`Error fetching playlist details: ${error}`);
+    }
 
-  return videos;
+    return videos;
 }
 
 let params = new URLSearchParams(document.location.search);
@@ -42,7 +41,53 @@ const playlistId = params.get("list");
 const apiKey = '<API-KEY>';
 
 getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
-  videos.forEach(video => {
-    console.log(`Title: ${video.title}, Video ID: ${video.id}`);
-  });
+    let board = document.querySelector('#board');
+    let rows = Math.ceil(videos.length/4);
+    for (let i = 0; i < rows; i++){
+        let row = `
+            <div class="card-deck" >
+        `
+        for (let j = 0; j < 4; j++) {
+            let index = i*4 + j;
+            if (index < videos.length) {
+                const video = videos[index];
+                // const element = array[index];
+                let videoElement = 
+                `<div class="card text-white bg-dark mb-3">
+                    <span id="player-${video.id}"></span>
+                    <img src="https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg" class="card-img-top" alt="...">
+                    <div class="card-img-overlay d-flex flex-column">
+                        <p class="card-title mb-auto">${video.title}</p>
+                    <div class="card-body d-flex align-items-end justify-content-around">
+                        <button type="button" class="btn btn-dark">
+                            <span id="play-pause-${video.id}">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+                                <!--
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+                                -->
+                            </span>
+                        </button>
+                        <button type="button" class="btn btn-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 6h12v12H6z"/></svg>
+                        </button>
+                        <button type="button" class="btn btn-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
+                        </button>
+                        <button type="button" class="btn btn-dark">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/></svg>
+                        </button>
+                        <!-- <span><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg></span> -->
+                    </div>
+                    </div>
+                </div>`;
+                    row += videoElement;
+            } else {
+                let videoElement = `<div class="card text-white bg-dark mb-3"></div>`;
+                row += videoElement;
+            }
+            
+        }
+        row += `</div>`;
+        board.innerHTML += row;
+    }
 });
