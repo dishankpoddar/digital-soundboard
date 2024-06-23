b45e870036ff8eff3ffb0753e8f43baf5f7eb754
Dishank Poddar <dishankpoddar@gmail.com>
2024-06-23 08:12:27 -0400

    Screen size based columns added

--- soundboard.js
+++ soundboard.js
@@ -45,14 +45,24 @@ let playerList = new Object();
 
 getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
     let board = document.querySelector('#board');
-    let rows = Math.ceil(videos.length/4);
+    let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    
+    let cols = 4;
+    if (vw < 768){
+        cols = 1;
+    } else if (vw < 1200){
+        cols = 3;
+    } else {
+        cols = 4;
+    }
+    let rows = Math.ceil(videos.length/cols);
     vcount = videos.length;
     for (let i = 0; i < rows; i++){
         let row = `
             <div class="card-deck" >
         `
-        for (let j = 0; j < 4; j++) {
-            let index = i*4 + j;
+        for (let j = 0; j < cols; j++) {
+            let index = i*cols + j;
             if (index < videos.length) {
                 const video = videos[index];
                 let videoElement = 
@@ -64,22 +74,22 @@ getYouTubePlaylistVideos(playlistId, apiKey).then(videos => {
                     <div class="card-body d-flex align-items-end justify-content-around">
                         <button type="button" class="btn btn-dark" onclick="playpauseVideo('${video.id}')">
                             <span id="play-pause-${video.id}">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
                                 <!--
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
                                 -->
                             </span>
                         </button>
                         <button type="button" class="btn btn-dark" onclick="stopVideo('${video.id}')">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 6h12v12H6z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 6h12v12H6z"/></svg>
                         </button>
                         <button type="button" class="btn btn-dark" onclick="volumeDown('${video.id}')">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M18.5 12c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM5 9v6h4l5 5V4L9 9H5z"/></svg>
                         </button>
                         <button type="button" class="btn btn-dark" onclick="volumeUp('${video.id}')">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
                         </button>
-                        <!-- <span><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg></span> -->
+                        <!-- <span><svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg></span> -->
                     </div>
                     </div>
                 </div>`;
@@ -120,7 +130,7 @@ function onPlayerReady(event) {
   ready += 1;
   if (ready == vcount){
     let alertBox = document.querySelector('#alertBox');
-    alertBox.classList.remove('alert-warning');
+    alertBox.classList.vwove('alert-warning');
     alertBox.classList.add('alert-success');
     alertBox.textContent = 'Playlist Loaded';
     setTimeout(function(){alertBox.style.display='none';}, 2000);
@@ -134,18 +144,18 @@ function playpauseVideo(video_id){
     let playpauseIcon = document.querySelector(`#play-pause-${video_id}`);
     if ( player.getPlayerState() == YT.PlayerState.PLAYING || player.getPlayerState() == YT.PlayerState.BUFFERING ) {
         player.pauseVideo();
-        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
+        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
     } 
     else {
         player.playVideo();  
-        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>`;
+        playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>`;
     } 
 }
 function stopVideo(video_id){
     let player = playerList[video_id];
     player.stopVideo();
     let playpauseIcon = document.querySelector(`#play-pause-${video_id}`);
-    playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 0 24 24" width="40px" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
+    playpauseIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="3vw" viewBox="0 0 24 24" width="3vw" fill="#DF9D9B"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>`;
 }
 function volumeUp(video_id){
     let player = playerList[video_id];
